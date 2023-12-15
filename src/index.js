1862b5fa095e523818ba517a387c27c7a5a56323
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-12-15 15:58:36 -0700

    Added api integration for poem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem() {
   new Typewriter("#poem", {
     strings: "Twiddle Dee Dee!",
     autoStart: true,
     delay: 75,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let userPrompt = document.querySelector("#userPrompt");
+  let apiKey = "d0c776oeta30c3dfb9d3ead51c9fd9a4";
+  let prompt = `Generate a silly limerick or poem about ${userPrompt.value}`;
+  let context =
+    "You have good knowledge about limericks and poems and can make up silly poems. Generate a 4 line poem or limerick for the user and separate each line with a <br/>. Sign the poem with 'By your AI buddy'";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemGenerator = document.querySelector("#generate");
