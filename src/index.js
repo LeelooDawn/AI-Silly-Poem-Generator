8f42531c656c25b18fa7183d6c08355a573b73de
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-12-15 16:03:25 -0700

    Updated placeholder in html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-function displayPoem() {
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Twiddle Dee Dee!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 75,
     cursor: "",
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "d0c776oeta30c3dfb9d3ead51c9fd9a4";
   let prompt = `Generate a silly limerick or poem about ${userPrompt.value}`;
   let context =
-    "You have good knowledge about limericks and poems and can make up silly poems. Generate a 4 line poem or limerick for the user and separate each line with a <br/>. Sign the poem with 'By your AI buddy'";
+    "You have good knowledge about limericks and poems and can make up silly poems. Generate a 4 line poem or limerick for the user and separate each line with a <br/>. Sign the poem with '~ By your AI buddy'";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
 }
