a1634dd88f78c09b8b914653b0934d47dbdb4573
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-12-15 15:41:10 -0700

    Basic Form Javascript handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Twiddle Dee Dee!",
+    autoStart: true,
+    delay: 75,
+    cursor: "",
+  });
+}
 
+let poemGenerator = document.querySelector("#generate");
+poemGenerator.addEventListener("submit", generatePoem);
