f7cb54231eaa6d7039edd0265f4ec8bec1efd2e9
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-01-19 15:21:17 -0700

    Added AI joke telling!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+let apiKey = "d0c776oeta30c3dfb9d3ead51c9fd9a4";
+
+let poemGenerator = document.querySelector("#generate");
+poemGenerator.addEventListener("submit", generatePoem);
+
 function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
@@ -10,7 +15,6 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let userPrompt = document.querySelector("#userPrompt");
-  let apiKey = "d0c776oeta30c3dfb9d3ead51c9fd9a4";
   let prompt = `Generate a silly limerick or poem about ${userPrompt.value}`;
   let context =
     "You have good knowledge about limericks and poems and can make up silly poems. Generate a 4 line poem or limerick for the user and separate each line with a <br/>. Sign the poem with '~ By your AI buddy'";
@@ -23,5 +27,26 @@ function generatePoem(event) {
   axios.get(apiUrl).then(displayPoem);
 }
 
-let poemGenerator = document.querySelector("#generate");
-poemGenerator.addEventListener("submit", generatePoem);
+let button = document.querySelector("#button");
+button.addEventListener("click", handleClick);
+
+function showAnswer(response) {
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 75,
+  });
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  let prompt = "Tell me a silly joke please!";
+  let context = "You seem funny, tell silly short jokes about animals";
+  let answer = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.classList.remove("hidden");
+  jokeElement.innerHTML = "Are you ready to laugh?";
+
+  axios.get(answer).then(showAnswer);
+}
